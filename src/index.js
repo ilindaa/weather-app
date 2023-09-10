d7cf05a21f33a84e7414c4a83b8f1aef1671a668
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-09-10 19:09:44 -0400

    Working on JS code (promise-based)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import './style.css';
 
-const test = document.createElement('p');
-test.textContent = 'Testing';
-document.body.appendChild(test);
+function testing() {
+  const test = document.createElement('p');
+  test.textContent = 'Testing';
+  document.body.appendChild(test);
+}
+
+function weather() {
+  const apiKey = ''; // This is a free public API key, will learn how to handle keys without pushing to front-end later
+  const location = 'japan';
+
+  fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
+    {
+      mode: 'cors',
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      console.log(response.location.name);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+testing();
+weather();
