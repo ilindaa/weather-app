0b20db9cf995175604df6eb85a9bebaea15a2902
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-09-10 22:43:54 -0400

    Updated promise-based code to async/await code

--- src/index.js
+++ src/index.js
@@ -6,25 +6,29 @@ function testing() {
   document.body.appendChild(test);
 }
 
-function weather() {
-  const apiKey = ''; // This is a free public API key, will learn how to handle keys without pushing to front-end later
-  const location = 'japan';
+// Take location and return the weather data for that location
+async function getWeatherData(str) {
+  const apiKey = '54491ce3d41e459d976173417231009'; // This is a free public API key, will learn how to handle keys without pushing to front-end later
+  const location = str;
 
-  fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
-    {
-      mode: 'cors',
-    }
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-      console.log(response.location.name);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
+      { mode: 'cors' }
+    );
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (err) {
+    console.log(err);
+  }
+  return null;
+}
+
+// Process the JSON data from the API and return an object with only the data required for the app
+function processWeatherData(weatherData) {
+  console.log(weatherData);
+  console.log(weatherData.location.name);
 }
 
 testing();
-weather();
+processWeatherData(await getWeatherData('japan'));
